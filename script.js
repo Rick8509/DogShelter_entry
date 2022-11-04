2a819b7f7142060e3d28e3e430b13360f443c44e
Rick Kar <rickkar64@gmail.com>
2022-11-04 09:23:10 +0530

    committed

--- script.js
+++ script.js
@@ -8,7 +8,47 @@ document.body.onload = () => {
 document.getElementById("createnode").onclick = () => {
   var newDiv = document.createElement("div");
   newDiv.classList.add("entries");
-  var inputtext = document.createElement("input");
-  newDiv.appendChild(inputtext);
+
+  var div1 = document.createElement("div");
+  var div2 = document.createElement("div");
+  var div3 = document.createElement("div");
+  var div4 = document.createElement("div");
+
+  div1.classList.add("formdivs");
+  div2.classList.add("formdivs");
+  div3.classList.add("formdivs");
+  div4.classList.add("formdivs");
+
+  var p1 = document.createElement("p");
+  p1.innerHTML = "Name :";
+  var inputtext1 = document.createElement("input");
+  inputtext1.setAttribute("type", "text");
+  div1.appendChild(p1);
+  div1.appendChild(inputtext1);
+
+  var p2 = document.createElement("p");
+  p2.innerHTML = "Contact Number :";
+  var inputtext2 = document.createElement("input");
+  inputtext2.setAttribute("type", "number");
+  div2.appendChild(p2);
+  div2.appendChild(inputtext2);
+
+  var p3 = document.createElement("p");
+  p3.innerHTML = "Contact Number :";
+  var inputdate = document.createElement("input");
+  inputdate.setAttribute("type", "date");
+  div3.appendChild(p3);
+  div3.appendChild(inputdate);
+
+  var p4 = document.createElement("p");
+  p4.innerHTML = "Days :";
+  var showday = document.createElement("div");
+  div4.appendChild(p4);
+  div4.appendChild(showday);
+
+  newDiv.appendChild(div1);
+  newDiv.appendChild(div2);
+  newDiv.appendChild(div3);
+  newDiv.appendChild(div4);
   document.getElementById("listcontainer").appendChild(newDiv);
 };
